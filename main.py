7ca8a09f524f3097f684b7c3aefdfe20644e68b6
SethPerko-GVSU <perkos@mail.gvsu.edu>
2025-02-17 16:11:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import font as tkfont
 from tkinter import *
 from client_release import PollClient
 from server_release import PollServer
+import threading
 
 class Chatroom(tk.Tk):
 
@@ -23,6 +24,8 @@ class Chatroom(tk.Tk):
             self.frames[page_name] = frame
 
             frame.grid(row=0, column=0, sticky="nsew")
+            
+        self.server = None
 
         the_menu = Menu(self)
         self.config(menu=the_menu)
@@ -39,6 +42,9 @@ class Chatroom(tk.Tk):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+        
+        if page_name == "Server_Page":
+            frame.update_server_info(self.server.host, self.server.port)
 
 
 class StartPage(tk.Frame):
@@ -50,7 +56,7 @@ class StartPage(tk.Frame):
         label.grid(row=0, column=1)
 
         button1 = tk.Button(self, text="Server",
-                            command=lambda: controller.show_frame("Server_Page"))
+                            command=lambda: start_server(controller))
         button2 = tk.Button(self, text="Client",
                             command=lambda: controller.show_frame("Client_Page"))
         button1.grid(row=2, column=0)
@@ -62,11 +68,12 @@ class Server_Page(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        self.label = tk.Label(self, text="Waiting on Server...", font=controller.title_font)
+        self.label.pack(side="top", fill="x", pady=10)
+        
+    def update_server_info(self, host, port):
+        # Update the label with server host and port.
+        self.label.config(text=f"Server running at {host}:{port}")
 
 
 class Client_Page(tk.Frame):
@@ -76,14 +83,18 @@ class Client_Page(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="This is page 2", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+
 
 def start_server(controller):
+    """Start the server in a separate thread to prevent UI freezing."""
+    def run_server():
+        controller.server = PollServer()
+        controller.server.start()
+
+    server_thread = threading.Thread(target=run_server, daemon=True)
+    server_thread.start()
+
     controller.show_frame("Server_Page")
-    server = PollServer()
-    server.start()
 
 
 if __name__ == "__main__":
