d3224084e97054362fb9828b0fae35120bc9e9f2
SethPerko-GVSU <perkos@mail.gvsu.edu>
2025-02-22 19:10:51 -0500

    Update server_release.py

--- server_release.py
+++ server_release.py
@@ -19,33 +19,28 @@ class PollServer:
             data = conn.recv(1024).decode()
             if not data:
                 break
-
             print(data)
-        
 
     def server_input(self):
         while True:
             message = f'{self.username}: {input()}'
             with self.lock:
                 for conn in self.connected_clients:
-                    try:
-                        conn.sendall(message.encode())
-                    except Exception as e:
-                        print(f"Error sending message: {e}")
+                    conn.sendall(message.encode())
 
     def start(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.host, self.port))
-        s.listen()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.host, self.port))
+        self.s.listen()
         print(f"Server listening on {self.host}:{self.port}")
-
 
         threading.Thread(target=self.server_input, daemon=True).start()
 
         while True:
-            conn, addr = s.accept()
+            conn, addr = self.s.accept()
             threading.Thread(target=self.handle_client, args=(conn, addr), daemon=True).start()
-
-if __name__ == "__main__":
-    server = PollServer()
-    server.start()
+            
+    def stop(self):
+        self.s.close()
+        
+        
