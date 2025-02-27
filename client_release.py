8f685ea49c7c4a77bb4e37f81184b831b2e34f8b
SethPerko-GVSU <perkos@mail.gvsu.edu>
2025-02-27 11:25:13 -0500

    Update client_release.py

--- client_release.py
+++ client_release.py
@@ -8,44 +8,44 @@ class PollClient:
         self.port = port
         self.username = username
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.message_queue = queue.Queue()  # Queue to hold messages from the GUI
-        self.running = True  # Control flag for stopping the client
+        self.message_queue = queue.Queue() # Line by ChatGPT
+        self.running = True
 
     def start(self):
-        """Starts the client and begins listening for messages."""
         self.client_socket.connect((self.host, self.port))
         print("Connected to the chatroom.\n")
 
-        # Start a thread to receive messages
         threading.Thread(target=self.receive_messages, daemon=True).start()
         
-        # Start a thread to send messages from the queue
         threading.Thread(target=self.process_messages, daemon=True).start()
 
 
+    ''' send_message() and process_message() were both created by ChatGPT and then tested using the unit tests.
+    These functions were not previously here as client_release only dealt with messages through the command line.
+    Once I tried to get it to work using the GUI in TKinter, I quickly realized that I was running to a lot of walls
+    trying to get it to send through the GUI instead of using input().
+    '''
+    
     def send_message(self, message):
-        """Sends a message from the GUI to the server."""
         if self.running:
             formatted_message = f"{self.username}: {message}"
-            self.message_queue.put(formatted_message)  # Add message to queue
+            self.message_queue.put(formatted_message)
 
     def process_messages(self):
-        """Processes messages from the queue and sends them to the server."""
         while self.running:
             if not self.message_queue.empty():
                 message = self.message_queue.get()
                 self.client_socket.sendall(message.encode())
                 
+    # Everything after this line is written by me
 
     def receive_messages(self):
-        """Continuously receives messages from the server."""
         while self.running:
                 data = self.client_socket.recv(1024)
                 if not data:
                     break
-                print(data.decode())  # Display received messages in console
+                print(data.decode())
 
     def stop(self):
-        """Stops the client gracefully."""
         self.running = False
         self.client_socket.close()
