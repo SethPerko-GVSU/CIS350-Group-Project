b31c08a792f0abf071fdf59c24121def7ffb710e
SethPerko-GVSU <perkos@mail.gvsu.edu>
2025-04-16 12:29:53 -0400

    adds timestamps and server display messages

--- server_release.py
+++ server_release.py
@@ -1,26 +1,44 @@
 import socket
 import threading
-import json
+import datetime
 
 class PollServer:
-    def __init__(self, host, port):
+    def __init__(self, host, port, update_callback=None):
         self.host = host
         self.port = port
         self.connected_clients = []  
         self.lock = threading.Lock()
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.username = 'SERVER_NAME'
+        self.update_callback = update_callback
 
     def handle_client(self, conn, addr):
-        print(f"Client connected from {addr[0], addr[1]}")
+        msg = f"Client connected from {addr[0]}:{addr[1]}"
+        print(msg)
+        
+        if self.update_callback:
+            self.update_callback(msg)
+        
         with self.lock:
             self.connected_clients.append(conn)
 
         while True:
-            data = conn.recv(1024).decode()
-            if not data:
+            try:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
+                
+                timestamp = datetime.datetime.now().strftime("[%I:%M %p]")
+                full_msg = f"{timestamp} {data}"
+                print(full_msg)
+                
+                with self.lock:
+                    for c in self.connected_clients:
+                        c.sendall(full_msg.encode())
+                    
+                if self.update_callback:
+                    self.update_callback(full_msg)
+            except:
                 break
-            print(data)
 
     def server_input(self):
         while True:
@@ -30,9 +48,13 @@ class PollServer:
                     conn.sendall(message.encode())
 
     def start(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind((self.host, self.port))
         self.s.listen()
         print(f"Server listening on {self.host}:{self.port}")
+        
+        if self.update_callback:
+            self.update_callback(f"Server listening on {self.host}:{self.port}")
 
         threading.Thread(target=self.server_input, daemon=True).start()
 
